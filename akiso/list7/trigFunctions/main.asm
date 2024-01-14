1781e3b5ad3748804e83323f7a02756d472fff00
kallazz <michael.w.kallas@gmail.com>
2024-01-14 16:22:47 +0100

    [akiso] Completed task 3 without custom conversion to floats

--- akiso/list7/trigFunctions/main.asm
+++ akiso/list7/trigFunctions/main.asm
@@ -2,27 +2,33 @@
 %include "../functions.asm"
 
 section .data
-prompt:             db "Enter a float: ", 0
+prompt:             db "Enter a number: ", 0
 promptFormat:       db "%s", 0
 inputFormat:        db "%lf", 0
-outputFormat:       db "%lf", 10, 0
-
 outSin:             db "sin(x) = %lf", 10, 0
 outCos:             db "cos(x) = %lf", 10, 0
 outSqrt:            db "sqrt(x) = %lf", 10, 0
 outExp:             db "exp(x) = %lf", 10, 0
 
 eValue:             dq 2.71828
+result:             dq 0.0
 
 section .bss
-inputFloat          resb 8
+userInput           resb 8
 
-jes
 section .text
-
 global main
 extern printf
 extern scanf
+
+printResult:
+    push    dword [result + 4]      ; push the higher 4 bytes
+    push    dword [result + 0]      ; push the lower 4 bytes
+    push    eax
+    call    printf
+    add     esp, 12
+    ret
+
 main:
     ; print the prompt
     push    prompt
@@ -30,70 +36,60 @@ main:
     call    printf
     add     esp, 8
 
-    push    inputFloat
+    ; store the input
+    push    userInput
     push    inputFormat
     call    scanf
-    add     esp, 8
+    add     esp, 12
 
-
+    ; initialize FPU
     finit
-    fld     qword [inputFloat]       ; load x to the stack
+
+    ; push input to the stack and duplicate it 3 times
+    fld     qword [userInput]
     fld     st0
     fld     st0
     fld     st0
 
+    ; sin(x)
     fsin
-    fst     qword [inputFloat]       ; store the top stack value at z, z = sqrt(x * x + y * y)
-    fstp    st0
-    ; print
-    push    dword [inputFloat + 4]  ; push the higher 4 bytes of float
-    push    dword [inputFloat + 0]  ; push the lower 4 bytes of float
-    push    outSin
-    call    printf
-    add     esp, 12
+    fstp    qword [result]
+    mov     eax, outSin
+    call    printResult
 
+    ; cos(x)
     fcos
-    fst     qword [inputFloat]
-    fstp    st0
-    ; print
-    push    dword [inputFloat + 4]  ; push the higher 4 bytes of float
-    push    dword [inputFloat + 0]  ; push the lower 4 bytes of float
-    push    outCos
-    call    printf
-    add     esp, 12
+    fstp    qword [result]
+    mov     eax, outCos
+    call    printResult
 
+    ; sqrt(x)
     fsqrt
-    fst     qword [inputFloat]
-    fstp    st0
-    ; print
-    push    dword [inputFloat + 4]  ; push the higher 4 bytes of float
-    push    dword [inputFloat + 0]  ; push the lower 4 bytes of float
-    push    outSqrt
-    call    printf
-    add     esp, 12
+    fstp    qword [result]
+    mov     eax, outSqrt
+    call    printResult
 
+    ; exp(x)
     fld     qword [eValue]
-    fld     st0
-    fmul
-    fst     qword [inputFloat]
-    fstp    st0
-    ; print
-    push    dword [inputFloat + 4]  ; push the higher 4 bytes of float
-    push    dword [inputFloat + 0]  ; push the lower 4 bytes of float
-    push    outExp
-    call    printf
-    add     esp, 12
 
+    fyl2x                       ; st0 = x * log2(e)
+    fld     st0                 ; duplicate
 
+    frndint                     ; get integer part
+    fxch                        ; swap st0 with st1
+    fsub    st0, st1            ; get fractional part
+    f2xm1                       ; 2^frac(x * log2(e))
+    fld1
+    faddp
 
+    fxch                        ; swap st0 with st1
+    fld1
+    fscale                      ; 1 * 2^int(x * log2(2))
+    fstp    st1
+    fmulp                       ; 2^frac(x * log2(e)) * 2^int(x * log2(e)) = 2^(x * log2(e))
 
+    fstp    qword [result]
+    mov     eax, outExp
+    call    printResult
 
-
-
-
-
-
-.finished:
-    pop ebx     ; restore the stack
-    ; finish with exit code 0
-    call quit
+    call    quit
