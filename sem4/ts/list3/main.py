4474512a42e8b381b2d87f59607777400bf831f7
kallazz <michael.w.kallas@gmail.com>
2024-05-13 23:14:50 +0200

    [ts] Added prints

--- sem4/ts/list3/main.py
+++ sem4/ts/list3/main.py
@@ -4,13 +4,23 @@ from bit_framing import encode, decode
 def main() -> None:
     with open("input.txt", "r") as input_file:
         input_bits = input_file.read()
+        print("Input bits:")
+        print(f"{input_bits}\n")
 
     with open("encoded.txt", "w") as encoded_file:
         encoded_bits = encode(input_bits)
+        print("Encoded bits:")
+        print(f"{encoded_bits}\n")
         encoded_file.write(encoded_bits)
 
     with open("decoded.txt", "w") as decoded_file:
-        decoded_file.write(decode(encoded_bits))
+        decoded_bits = decode(encoded_bits)
+        print("Decoded bits:")
+        print(f"{decoded_bits}\n")
+        decoded_file.write(decoded_bits)
+
+    if input_bits == decoded_bits:
+        print("The bits were successfully decoded!")
 
 
 if __name__ == "__main__":
