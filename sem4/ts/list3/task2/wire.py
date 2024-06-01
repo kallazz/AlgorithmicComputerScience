27e6be13e0d0822219c0d8613ddb6d6f3164e75c
kallazz <michael.w.kallas@gmail.com>
2024-06-01 12:53:50 +0200

    [ts] Added some improvements

--- sem4/ts/list3/task2/wire.py
+++ sem4/ts/list3/task2/wire.py
@@ -3,9 +3,9 @@ from dataclasses import dataclass
 
 
 class Wire:
-    @dataclass(frozen=True)
+    @dataclass
     class Signal:
-        index: str
+        symbol: str
         left_position: int
         right_position: int
         jam: bool = False
@@ -16,7 +16,7 @@ class Wire:
 
     def __init__(self, length: int, tick_time_ms: int) -> None:
         self._wire_segments = [0] * length
-        self._signals = set()
+        self._signals = []
         self._tick_time_ms = tick_time_ms
 
     @property
@@ -25,22 +25,25 @@ class Wire:
 
     def tick(self) -> None:
         for signal in self._signals:
-            self._wire_segments[signal.left_position] = signal.index
-            self._wire_segments[signal.right_position] = signal.index
-            signal.left_position += 1
+            self._wire_segments[signal.left_position] = signal.symbol
+            self._wire_segments[signal.right_position] = signal.symbol
+            signal.left_position -= 1
             signal.right_position += 1
 
     def add_signal(self, device_position: int, signal_symbol: str) -> None:
-        self._signals.add(self.Signal(signal_symbol, device_position, device_position))
+        self._signals.append(self.Signal(signal_symbol, device_position, device_position))
 
-    def remove_signal(self, device_position: int, signal_symbol: str) -> None:
-        self._signals.remove(self.Signal(signal_symbol, device_position, device_position))
+    def remove_signal(self, signal_symbol: str) -> None:
+        for i, signal in enumerate(self._signals):
+            if signal.symbol == signal_symbol:
+                self._signals.pop(i)
+                return
 
     def add_jam_signal(self, device_position: int) -> None:
-        self._signals.add(self.Signal(self.JAM_SIGNAL_SYMBOL, device_position, device_position, True))
+        self._signals.append(self.Signal(self.JAM_SIGNAL_SYMBOL, device_position, device_position, True))
 
     def remove_jam_signal(self, device_position: int) -> None:
-        self._signals.add(self.Signal(self.JAM_SIGNAL_SYMBOL, device_position, device_position, True))
+        self._signals.remove(self.Signal(self.JAM_SIGNAL_SYMBOL, device_position, device_position, True))
 
     def is_free(self, position: int, signal_symbol: str) -> bool:
         return self._wire_segments[position] != self.DEFAULT_SIGNAL and self._wire_segments[position] != signal_symbol
