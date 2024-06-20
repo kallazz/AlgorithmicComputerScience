520ed9fefcaaaae169bfb4aa32ca5d34c5dad1c8
kallazz <michael.w.kallas@gmail.com>
2024-06-20 17:51:26 +0200

    [ts] Added list 5

---
+++ sem4/ts/list5/server.pl
@@ -0,0 +1,37 @@
+use HTTP::Daemon;
+use HTTP::Status;
+
+my $daemon = HTTP::Daemon->new(
+    LocalAddr => "localhost",
+    LocalPort => 4321,
+) || die;
+
+print "Running at: ", $daemon->url, "\n";
+
+while (my $connection = $daemon->accept) {
+    while (my $request = $connection->get_request) {
+        if ($request->method ne "GET") {
+            $connection->send_error(RC_FORBIDDEN);
+            next;
+        }
+
+        my $uri = $request->uri;
+        if ($uri->path eq "/header") {
+            my $headers = $request->headers_as_string;
+            my $response = HTTP::Response->new(200);
+            $response->header("Content-Type" => "text/plain");
+            $response->content("Your request's header is:\n\n" . $headers);
+            $connection->send_response($response);
+        } else {
+            if ($uri->path eq "/") {
+                $uri = "/index";
+            }
+
+            my $filename = "websites" . $uri . ".html";
+            $connection->send_file_response($filename);
+        }
+    }
+
+    $connection->close;
+    undef($connection);
+}
